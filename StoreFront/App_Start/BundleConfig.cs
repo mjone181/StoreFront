b0ba22f19a4cef213c66ab047510be54a4402af7
Mack Jones <jonesmacka@outlook.com>
2020-11-19 20:56:24 -0600

    Added CSS and JS from template into Bundles and put the CSS bundle into Layout.

--- StoreFront/App_Start/BundleConfig.cs
+++ StoreFront/App_Start/BundleConfig.cs
@@ -18,13 +18,26 @@ namespace StoreFront.UI.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/Content/assets/js").Include(
+                      "~/Content/assets/js/bootstrap.min.js",
+                      "~/Content/assets/js/respond.min.js",
+                      "~/Content/assets/js/jquery.js",
+                      "~/Content/assets/js/html5shiv.js",
+                      "~/Content/assets/js/modernizr.custom.js",
+                      "~/Content/assets/js/classie.js",
+                      "~/Content/assets/js/plugins.js",
+                      "~/Content/assets/js/prettyPhoto.js",
+                      "~/Content/assets/js/imagesloaded.js",
+                      "~/Content/assets/js/respond.min.js",
+                      "~/Content/assets/js/init.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/assets/css").Include(
+                      "~/Content/assets/css/bootstrap.css",
+                      "~/Conent/assets/css/font-awesome.min.css",
+                      "~/Content/assets/css/animate.css",
+                      "~/Content/assets/css/pe-icons.css",
+                      "~/Content/assets/css/prettyPhoto.css",
+                      "~/Content/assets/css/style.css"));
         }
     }
 }
